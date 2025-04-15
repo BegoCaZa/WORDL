a27bd7414cb741dab542ed3955a31cb431608f1d
BegoCaZa <begoxcz@hotmail.com>
2025-04-15 15:02:13 +0200

    ADRIAN ESTOY UPER CONFUNDIDA POR FAVOR NO DEJES MAS TRABAJOS PARA SEMANA SANTA

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 //traer variables css
 const rootStyles = document.documentElement.style;
+
+//DOM
+const gameboardElement = document.getElementById('gameboard');
+
 // form
 const formElement = document.getElementById('user-solution');
 //obejto palabras
@@ -22,17 +26,46 @@ const ALL_WORDS = [
   'cordyceps'
 ];
 
+let numberOfAttempts = 0;
+
 //FUNCIONES
 
 const validateWord = event => {
   event.preventDefault();
 };
 
-const getRandomWord = ALL_WORDS => {
+const getRandomWord = () => {
   const randomIndex = Math.floor(Math.random() * ALL_WORDS.length);
-  return ALL_WORDS[randomIndex];
+  const randomWord = ALL_WORDS[randomIndex];
+  return randomWord;
 };
-console.log(getRandomWord());
+
+const createGameboard = () => {
+  const word = getRandomWord();
+  console.log(word);
+  const letters = word.split('');
+  
+  for (let i = 0; i < 5; i++) {
+    
+  const fragment = document.createDocumentFragment();
+
+  letters.forEach(letter => {
+    const letterElement = document.createElement('span');
+    // letterElement.textContent = letter;
+    letterElement.classList.add('letter-container');
+    fragment.append(letterElement);
+  });
+
+  const rowContainerElement= document.createElement('div');
+  rowContainerElement.classList.add('gameboard-row');
+  rowContainerElement.append(fragment);
+
+  gameboardElement.append(rowContainerElement);
+  gameboardElement.classList.add('gameboard');
+  }
+}
+
+createGameboard();
 
 //EVENTOS
 formElement.addEventListener('submit', validateWord);
